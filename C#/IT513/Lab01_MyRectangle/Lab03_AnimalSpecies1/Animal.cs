a6869f3df08b4d6934a52cff04805011f334ee26
Kevin-kdW <k.r.young192@gmail.com>
2026-05-30 18:38:50 +1200

    Lab03

---
+++ C#/IT513/Lab01_MyRectangle/Lab03_AnimalSpecies1/Animal.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab03_AnimalSpecies1
+{
+    internal class Animal
+    {
+		private string species;
+
+		public string Species
+		{
+			get { return species; }
+			set { species = value; }
+		}
+
+		private int legs;
+
+		public int Legs
+		{
+			get { return legs; }
+			set { legs = value; }
+		}
+
+		private string classification;
+
+		public string Classification
+		{
+			get { return classification; }
+			set { classification = value; }
+		}
+
+
+		private bool isExtinct;
+
+		public bool IsExtinct
+		{
+			get { return isExtinct; }
+			set { isExtinct = value; }
+		}
+
+        public Animal(string species, int legs, string classification, bool isExtinct)
+        {
+			Species = species;
+			Legs = legs;
+			Classification = classification;
+			IsExtinct = isExtinct;
+        }
+
+        public override string ToString()
+        {
+            if (legs == 2 && isExtinct == true)
+			{
+                return $"{species} ({legs}) {classification} ({isExtinct})"; 
+			}
+			else
+			{
+                return $"{species} ({legs}) {classification}";
+            }
+        }
+    }
+}
---
+++ C#/IT513/Lab01_MyRectangle/Lab03_AnimalSpecies1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Lab03_AnimalSpecies1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("513 Lab03\n");
+
+            using (StreamReader reader = new StreamReader(@"animals.txt"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    string[] parts = line.Split(",");
+                    if (line == "") continue;
+                    string part1 = parts[0];
+                    int part2 = int.Parse(parts[1]);
+                    string part3 = parts[2];
+                    bool part4 = bool.Parse(parts[3]);
+                    Animal animal = new Animal(part1,part2,part3,part4);
+                    Console.WriteLine(animal);
+                }
+            }
+        }
+    }
+}
