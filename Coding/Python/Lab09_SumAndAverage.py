a81384656722e11c2eb926b84483e5c688218c9c
Kevin-kdW <k.r.young192@gmail.com>
2026-08-13 21:32:11 +1200

    Intial

---
+++ Coding/Python/Lab09_SumAndAverage.py
@@ -0,0 +1,10 @@
+line = int(input("Number: "))
+count = 0
+num = 0
+while line != 999:
+    num += line
+    count += 1
+    line = int(input("Number: "))
+
+
+print(f"Sum: {num}\nAverage: {num/count:.2f}")
